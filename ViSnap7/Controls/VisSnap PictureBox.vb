f77ee8f683b7a4497a632b6ca6bbd643f7abaed5
igutiez <igutiez@gmail.com>
2021-08-14 01:32:43 +0200

    Añadido PictureBox

---
+++ ViSnap7/Controls/VisSnap PictureBox.vb
@@ -0,0 +1,366 @@
+﻿Imports System.ComponentModel
+Imports System.ComponentModel.Design
+Imports System.Windows.Forms.Design
+
+''' <summary>
+''' PLC PictureBox control 
+''' </summary>
+<System.ComponentModel.Designer(GetType(PLCPictureBoxDesigner))>
+Class VS7_PictureBox
+    Inherits PictureBox
+    Private _PLC As Integer
+    Private _DataArea As General.DataArea = DataArea.DB
+    Private _DB As Integer
+    Private _Byte As Integer
+    Private _Bit As Integer
+    Private _DataType As General.DataType = DataType.BOOL
+    Private _Length As Integer
+    Private _txt As String
+    Private _PictureTrue As System.Drawing.Bitmap
+    Private _PictureFalse As System.Drawing.Bitmap
+
+    Public pLC_Value As Boolean = False
+
+
+
+#Region "PLC Properties"
+
+    <System.ComponentModel.Category(KPlcPropertiesCategory), System.ComponentModel.Description(KPlcNumberLabel)>
+    Public Property PLC_Number As Integer
+        Get
+            Return _PLC
+        End Get
+        Set(value As Integer)
+            _PLC = value
+        End Set
+    End Property
+    <System.ComponentModel.Category(KPlcPropertiesCategory), System.ComponentModel.Description(KPlcDataAreaLabel)>
+    Public Property PLC_DataArea As General.DataArea
+        Get
+            Return _DataArea
+        End Get
+        Set(value As General.DataArea)
+            _DataArea = value
+        End Set
+    End Property
+    <System.ComponentModel.Category(KPlcPropertiesCategory), System.ComponentModel.Description(KPlcDBLabel)>
+    Public Property PLC_DB As Integer
+        Get
+            Return _DB
+        End Get
+        Set(value As Integer)
+            _DB = value
+        End Set
+    End Property
+    <System.ComponentModel.Category(KPlcPropertiesCategory), System.ComponentModel.Description(KPlcByteLabel)>
+    Public Property PLC_Byte As Integer
+        Get
+            Return _Byte
+        End Get
+        Set(value As Integer)
+            _Byte = value
+        End Set
+    End Property
+    <System.ComponentModel.Category(KPlcPropertiesCategory), System.ComponentModel.Description(KPlcValueTypeLabel)>
+    Public Property PLC_DataType As General.DataType
+        Get
+            Return _DataType
+        End Get
+        Set(value As General.DataType)
+            _DataType = value
+
+        End Set
+    End Property
+    <System.ComponentModel.Category(KPlcPropertiesCategory), System.ComponentModel.Description(KPlcBitLabel)>
+    Public Property PLC_Bit As Integer
+        Get
+            Return _Bit
+        End Get
+        Set(value As Integer)
+            _Bit = value
+        End Set
+    End Property
+    <System.ComponentModel.Category(KPlcPropertiesCategory), System.ComponentModel.Description(KPlcLengthLabel)>
+    Public Property PLC_Length As Integer
+        Get
+            Return _Length
+        End Get
+        Set(value As Integer)
+            _Length = value
+        End Set
+    End Property
+    <System.ComponentModel.Category(KPlcPropertiesCategory), System.ComponentModel.Description(KPlcDBLabel)>
+    Public Property PLC_PictureTrue As System.Drawing.Bitmap
+        Get
+            Return _PictureTrue
+        End Get
+        Set(value As System.Drawing.Bitmap)
+            _PictureTrue = value
+        End Set
+    End Property
+    <System.ComponentModel.Category(KPlcPropertiesCategory), System.ComponentModel.Description(KPlcDBLabel)>
+    Public Property PLC_PictureFalse As System.Drawing.Bitmap
+        Get
+            Return _PictureFalse
+        End Get
+        Set(value As System.Drawing.Bitmap)
+            _PictureFalse = value
+        End Set
+    End Property
+
+
+    Sub New()
+        Me.Size = New Point(75, 75)
+        Me.BorderStyle = Windows.Forms.BorderStyle.FixedSingle
+        Me.BackgroundImageLayout = ImageLayout.Stretch
+
+
+    End Sub
+#End Region
+#Region "Control Events"
+
+
+
+#End Region
+#Region "Control Procedures"
+
+
+
+#End Region
+#Region "Plc reading and writing"
+    Public Sub UpdateValue(ByRef _PLC As PlcClient)
+
+
+        Select Case Me.PLC_DataArea
+            Case DataArea.DB
+                Me.pLC_Value = TakeValue(_PLC.dbData(Me.PLC_DB), Me.PLC_DB, Me.PLC_Byte, Me.PLC_Bit, Me.PLC_DataType, Me.PLC_Length)
+
+            Case DataArea.INPUT
+                Me.pLC_Value = TakeValue(_PLC.inputData(0), Me.PLC_DB, Me.PLC_Byte, Me.PLC_Bit, Me.PLC_DataType, Me.PLC_Length)
+
+            Case DataArea.MARK
+                Me.pLC_Value = TakeValue(_PLC.marksData(0), Me.PLC_DB, Me.PLC_Byte, Me.PLC_Bit, Me.PLC_DataType, Me.PLC_Length)
+
+            Case DataArea.OUTPUT
+                Me.pLC_Value = TakeValue(_PLC.outputData(0), Me.PLC_DB, Me.PLC_Byte, Me.PLC_Bit, Me.PLC_DataType, Me.PLC_Length)
+
+            Case Else
+        End Select
+
+        If Me.pLC_Value Then
+            Me.BackgroundImage = Me.PLC_PictureTrue
+        Else
+            Me.BackgroundImage = Me.PLC_PictureFalse
+        End If
+
+    End Sub
+
+
+
+
+
+
+    Private Function TakeValue(_DBData As PlcClient.ByteData, _PLC_DB As Integer, _PLC_Byte As Integer, _PLC_Bit As Integer, _PLC_DataType As Integer, _PLC_Length As Integer) As String
+        Dim txt As String = ""
+        Select Case _PLC_DataType
+            Case DataType.BOOL
+                txt = ViSnap7.S7.GetBitAt(_DBData.data, _PLC_Byte, _PLC_Bit)
+
+            Case DataType.CHR
+                txt = ViSnap7.S7.GetWCharsAt(_DBData.data, _PLC_Byte, 1)
+            Case DataType.DINT
+                txt = ViSnap7.S7.GetDIntAt(_DBData.data, _PLC_Byte)
+            Case DataType.INT
+                txt = ViSnap7.S7.GetIntAt(_DBData.data, _PLC_Byte)
+            Case DataType.REAL
+                txt = ViSnap7.S7.GetRealAt(_DBData.data, _PLC_Byte)
+            Case DataType.SINT
+                txt = ViSnap7.S7.GetSIntAt(_DBData.data, _PLC_Byte)
+            Case DataType.STR
+                txt = ViSnap7.S7.GetStringAt(_DBData.data, _PLC_Byte)
+            Case DataType.UINT
+                txt = ViSnap7.S7.GetUIntAt(_DBData.data, _PLC_Byte)
+            Case Else
+                txt = ""
+        End Select
+        Return txt
+    End Function
+
+#End Region
+
+End Class
+
+#Region "PictureBox Smart tags"
+
+Public Class PLCPictureBoxDesigner
+    Inherits ControlDesigner
+
+    Private _actionListCollection As DesignerActionListCollection
+
+    Public Overrides ReadOnly Property ActionLists() As System.ComponentModel.Design.DesignerActionListCollection
+        Get
+            If _actionListCollection Is Nothing Then
+                _actionListCollection = New DesignerActionListCollection()
+                _actionListCollection.Add(New PLCPictureBoxActionList(Me.Control))
+            End If
+
+            Return _actionListCollection
+        End Get
+    End Property
+End Class
+
+Friend Class PLCPictureBoxActionList
+    Inherits DesignerActionList
+
+    Private ctr As VS7_PictureBox
+    Private designerActionSvc As DesignerActionUIService
+
+    Public Sub New(ByVal component As IComponent)
+        MyBase.New(component)
+
+        ctr = DirectCast(component, VS7_PictureBox)
+        designerActionSvc = CType(GetService(GetType(DesignerActionUIService)), DesignerActionUIService)
+    End Sub
+
+#Region " Properties to display in the Smart-Tag panel "
+
+    Public Property PLC_DataArea As General.DataArea
+        Get
+            Return ctr.PLC_DataArea
+        End Get
+        Set(value As General.DataArea)
+            GetPropertyByName(ctr, "PLC_DataArea").SetValue(ctr, value)
+            designerActionSvc.Refresh(ctr)
+        End Set
+    End Property
+
+
+
+    Public Property PLC_Number() As Integer
+        Get
+            Return ctr.PLC_Number
+        End Get
+        Set(ByVal value As Integer)
+            GetPropertyByName(ctr, "PLC_Number").SetValue(ctr, value)
+            designerActionSvc.Refresh(ctr)
+
+        End Set
+    End Property
+
+    Public Property PLC_DB() As Integer
+        Get
+            Return ctr.PLC_DB
+        End Get
+        Set(ByVal value As Integer)
+            GetPropertyByName(ctr, "PLC_DB").SetValue(ctr, value)
+            designerActionSvc.Refresh(ctr)
+
+        End Set
+    End Property
+
+    Public Property PLC_Byte() As Integer
+        Get
+            Return ctr.PLC_Byte
+        End Get
+        Set(ByVal value As Integer)
+            GetPropertyByName(ctr, "PLC_Byte").SetValue(ctr, value)
+            designerActionSvc.Refresh(ctr)
+
+        End Set
+    End Property
+
+    Public Property PLC_Bit() As Integer
+        Get
+            Return ctr.PLC_Bit
+        End Get
+        Set(ByVal value As Integer)
+            GetPropertyByName(ctr, "PLC_Bit").SetValue(ctr, value)
+            designerActionSvc.Refresh(ctr)
+
+        End Set
+    End Property
+
+
+
+    Public Property PLC_PictureTrue() As System.Drawing.Bitmap
+        Get
+            Return ctr.PLC_PictureTrue
+        End Get
+        Set(ByVal value As System.Drawing.Bitmap)
+            GetPropertyByName(ctr, "PLC_PictureTrue").SetValue(ctr, value)
+            designerActionSvc.Refresh(ctr)
+
+        End Set
+    End Property
+
+    Public Property PLC_PictureFalse() As System.Drawing.Bitmap
+        Get
+            Return ctr.PLC_PictureFalse
+        End Get
+        Set(ByVal value As System.Drawing.Bitmap)
+            GetPropertyByName(ctr, "PLC_PictureFalse").SetValue(ctr, value)
+            designerActionSvc.Refresh(ctr)
+
+        End Set
+    End Property
+
+
+
+#End Region
+
+#Region " Methods to display in the Smart-Tag panel "
+
+    Public Sub OnDock()
+        If ctr.Dock = DockStyle.Fill Then
+            ctr.Dock = DockStyle.None
+        Else
+            ctr.Dock = DockStyle.Fill
+        End If
+
+        designerActionSvc.Refresh(ctr)
+    End Sub
+    Private Sub MarkFalsePictureAsDefault()
+        ctr.BackgroundImage = Me.PLC_PictureFalse
+    End Sub
+
+    Private Sub MarkTruePictureAsDefault()
+        ctr.BackgroundImage = Me.PLC_PictureTrue
+    End Sub
+#End Region
+
+
+    Public Overrides Function GetSortedActionItems() As System.ComponentModel.Design.DesignerActionItemCollection
+        Dim str As String
+        Dim items As New DesignerActionItemCollection
+
+        If ctr.Dock = DockStyle.Fill Then
+            str = "Undock in parent container."
+        Else
+            str = "Dock in parent container."
+        End If
+
+        'Add a few Header Items (categories)
+        items.Add(New DesignerActionHeaderItem(KPlcAdressingCategory))
+        items.Add(New DesignerActionHeaderItem(KPlcPictureBoxCategory))
+
+        'Add the properties
+        items.Add(New DesignerActionPropertyItem("PLC_DataArea", KPlcValueTypeLabel, KPlcAdressingCategory, KPlcTipDataArea))
+        items.Add(New DesignerActionPropertyItem("PLC_Number", KPlcNumberLabel, KPlcAdressingCategory, KPlcTipPlcNumber))
+        If PLC_DataArea = DataArea.DB Then
+            items.Add(New DesignerActionPropertyItem("PLC_DB", KPlcDBLabel, KPlcAdressingCategory, KPlcTipPlcDB))
+        End If
+        items.Add(New DesignerActionPropertyItem("PLC_Byte", KPlcByteLabel, KPlcAdressingCategory, KPlcTipPlcByte))
+
+        items.Add(New DesignerActionPropertyItem("PLC_Bit", KPlcBitLabel, KPlcAdressingCategory, KPlcTipPlcBit))
+        items.Add(New DesignerActionPropertyItem("PLC_PictureTrue", KPlcBitPictureTrue, KPlcPictureBoxCategory, KPlcTipPictureBoxTrue))
+        items.Add(New DesignerActionPropertyItem("PLC_PictureFalse", KPlcBitPictureFalse, KPlcPictureBoxCategory, KPlcTipPictureBoxFalse))
+
+        items.Add(New DesignerActionMethodItem(Me, "MarkFalsePictureAsDefault", KShowPictureFalseAsDefault))
+        items.Add(New DesignerActionMethodItem(Me, "MarkTruePictureAsDefault", KShowPictureTrueAsDefault))
+
+        'Return the ActionItemCollection
+        Return items
+    End Function
+End Class
+
+#End Region
