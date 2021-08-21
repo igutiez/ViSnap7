bdd741bc442aeaf767a9394e2c66946a4d005456
igutiez <igutiez@gmail.com>
2021-08-21 17:40:56 +0200

    Load values to Form

--- ViSnap7/Controls/ViSnap7 LoadSaveForm/LoadDialog.Designer.vb
+++ ViSnap7/Controls/ViSnap7 LoadSaveForm/LoadDialog.Designer.vb
@@ -38,19 +38,21 @@ Partial Class LoadDialog
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 0)
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(277, 274)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(369, 337)
+        Me.TableLayoutPanel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 1
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(146, 29)
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(195, 36)
         Me.TableLayoutPanel1.TabIndex = 0
         '
         'OK_Button
         '
         Me.OK_Button.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.OK_Button.Location = New System.Drawing.Point(3, 3)
+        Me.OK_Button.Location = New System.Drawing.Point(4, 4)
+        Me.OK_Button.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.OK_Button.Name = "OK_Button"
-        Me.OK_Button.Size = New System.Drawing.Size(67, 23)
+        Me.OK_Button.Size = New System.Drawing.Size(89, 28)
         Me.OK_Button.TabIndex = 0
         Me.OK_Button.Text = "Aceptar"
         '
@@ -58,41 +60,46 @@ Partial Class LoadDialog
         '
         Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.Cancel_Button.Location = New System.Drawing.Point(76, 3)
+        Me.Cancel_Button.Location = New System.Drawing.Point(101, 4)
+        Me.Cancel_Button.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Cancel_Button.Name = "Cancel_Button"
-        Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
+        Me.Cancel_Button.Size = New System.Drawing.Size(89, 28)
         Me.Cancel_Button.TabIndex = 1
         Me.Cancel_Button.Text = "Cancelar"
         '
         'RecipeList
         '
         Me.RecipeList.FormattingEnabled = True
-        Me.RecipeList.Location = New System.Drawing.Point(46, 64)
+        Me.RecipeList.ItemHeight = 16
+        Me.RecipeList.Location = New System.Drawing.Point(61, 79)
+        Me.RecipeList.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.RecipeList.Name = "RecipeList"
-        Me.RecipeList.Size = New System.Drawing.Size(317, 134)
+        Me.RecipeList.Size = New System.Drawing.Size(421, 164)
         Me.RecipeList.TabIndex = 1
         '
         'RecipeListLabel
         '
         Me.RecipeListLabel.AutoSize = True
         Me.RecipeListLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RecipeListLabel.Location = New System.Drawing.Point(158, 27)
+        Me.RecipeListLabel.Location = New System.Drawing.Point(211, 33)
+        Me.RecipeListLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.RecipeListLabel.Name = "RecipeListLabel"
-        Me.RecipeListLabel.Size = New System.Drawing.Size(99, 16)
+        Me.RecipeListLabel.Size = New System.Drawing.Size(121, 20)
         Me.RecipeListLabel.TabIndex = 2
         Me.RecipeListLabel.Text = "RECIPE LIST"
         '
         'LoadDialog
         '
         Me.AcceptButton = Me.OK_Button
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.Cancel_Button
-        Me.ClientSize = New System.Drawing.Size(435, 315)
+        Me.ClientSize = New System.Drawing.Size(580, 388)
         Me.Controls.Add(Me.RecipeListLabel)
         Me.Controls.Add(Me.RecipeList)
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "LoadDialog"
--- ViSnap7/Controls/ViSnap7 Textbox.vb
+++ ViSnap7/Controls/ViSnap7 Textbox.vb
@@ -299,6 +299,11 @@ Class VS7_Textbox
 
         End If
     End Sub
+
+    Public Sub updateValueFromForm(ByVal value As String)
+        Me.pLC_Value = value
+        Me.Text = value
+    End Sub
     Private Sub WriteValue(_Text As String, _PLC_Number As Integer, _PLC_DataArea As DataArea, _DataType As DataType, _DB As Integer, _Byte As Integer, _Bit As Integer, _Length As Integer)
         Select Case Me.PLC_DataArea
             Case DataArea.DB
