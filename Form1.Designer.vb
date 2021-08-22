44551f0461213077951111df8b3a2ab069cce170
igutiez <igutiez@gmail.com>
2021-08-22 22:22:31 +0200

    Actualizar controles y método de lectura de controles

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,151 +22,18 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.VS7_Textbox4 = New Proyecto_ViSnap7.VS7_Textbox()
-        Me.ViSnap7_LoadSaveForm2 = New Proyecto_ViSnap7.ViSnap7_LoadSaveForm()
-        Me.VS7_Textbox3 = New Proyecto_ViSnap7.VS7_Textbox()
-        Me.VS7_Textbox2 = New Proyecto_ViSnap7.VS7_Textbox()
-        Me.VS7_Textbox1 = New Proyecto_ViSnap7.VS7_Textbox()
-        Me.ViSnap7_LoadSaveForm1 = New Proyecto_ViSnap7.ViSnap7_LoadSaveForm()
-        Me.VS7_Checkbox1 = New Proyecto_ViSnap7.VS7_Checkbox()
         Me.SuspendLayout()
-        '
-        'VS7_Textbox4
-        '
-        Me.VS7_Textbox4.Location = New System.Drawing.Point(191, 69)
-        Me.VS7_Textbox4.Name = "VS7_Textbox4"
-        Me.VS7_Textbox4.PLC_Bit = 0
-        Me.VS7_Textbox4.PLC_Byte = 6
-        Me.VS7_Textbox4.PLC_DataArea = Proyecto_ViSnap7.General.DataArea.DB
-        Me.VS7_Textbox4.PLC_DataType = Proyecto_ViSnap7.General.DataType.STR
-        Me.VS7_Textbox4.PLC_DB = 1
-        Me.VS7_Textbox4.PLC_FormActive = True
-        Me.VS7_Textbox4.PLC_FormNumber = 1
-        Me.VS7_Textbox4.PLC_Length = 38
-        Me.VS7_Textbox4.PLC_Number = 0
-        Me.VS7_Textbox4.Size = New System.Drawing.Size(100, 22)
-        Me.VS7_Textbox4.TabIndex = 5
-        '
-        'ViSnap7_LoadSaveForm2
-        '
-        Me.ViSnap7_LoadSaveForm2.Extension = "dat"
-        Me.ViSnap7_LoadSaveForm2.LoadForm = True
-        Me.ViSnap7_LoadSaveForm2.Location = New System.Drawing.Point(171, 196)
-        Me.ViSnap7_LoadSaveForm2.Margin = New System.Windows.Forms.Padding(4)
-        Me.ViSnap7_LoadSaveForm2.Name = "ViSnap7_LoadSaveForm2"
-        Me.ViSnap7_LoadSaveForm2.PLC_FormNumber = 1
-        Me.ViSnap7_LoadSaveForm2.SaveFolder = "C:\recipes"
-        Me.ViSnap7_LoadSaveForm2.Size = New System.Drawing.Size(100, 28)
-        Me.ViSnap7_LoadSaveForm2.TabIndex = 4
-        Me.ViSnap7_LoadSaveForm2.Text = "ViSnap7_LoadSaveForm2"
-        '
-        'VS7_Textbox3
-        '
-        Me.VS7_Textbox3.Location = New System.Drawing.Point(591, 124)
-        Me.VS7_Textbox3.Margin = New System.Windows.Forms.Padding(4)
-        Me.VS7_Textbox3.Name = "VS7_Textbox3"
-        Me.VS7_Textbox3.PLC_Bit = 0
-        Me.VS7_Textbox3.PLC_Byte = 410
-        Me.VS7_Textbox3.PLC_DataArea = Proyecto_ViSnap7.General.DataArea.MARK
-        Me.VS7_Textbox3.PLC_DataType = Proyecto_ViSnap7.General.DataType.REAL
-        Me.VS7_Textbox3.PLC_DB = 1
-        Me.VS7_Textbox3.PLC_FormActive = True
-        Me.VS7_Textbox3.PLC_FormNumber = 1
-        Me.VS7_Textbox3.PLC_Length = 0
-        Me.VS7_Textbox3.PLC_Number = 0
-        Me.VS7_Textbox3.Size = New System.Drawing.Size(132, 22)
-        Me.VS7_Textbox3.TabIndex = 3
-        '
-        'VS7_Textbox2
-        '
-        Me.VS7_Textbox2.Location = New System.Drawing.Point(399, 124)
-        Me.VS7_Textbox2.Margin = New System.Windows.Forms.Padding(4)
-        Me.VS7_Textbox2.Name = "VS7_Textbox2"
-        Me.VS7_Textbox2.PLC_Bit = 0
-        Me.VS7_Textbox2.PLC_Byte = 402
-        Me.VS7_Textbox2.PLC_DataArea = Proyecto_ViSnap7.General.DataArea.MARK
-        Me.VS7_Textbox2.PLC_DataType = Proyecto_ViSnap7.General.DataType.INT
-        Me.VS7_Textbox2.PLC_DB = 1
-        Me.VS7_Textbox2.PLC_FormActive = True
-        Me.VS7_Textbox2.PLC_FormNumber = 1
-        Me.VS7_Textbox2.PLC_Length = 0
-        Me.VS7_Textbox2.PLC_Number = 0
-        Me.VS7_Textbox2.Size = New System.Drawing.Size(132, 22)
-        Me.VS7_Textbox2.TabIndex = 2
-        '
-        'VS7_Textbox1
-        '
-        Me.VS7_Textbox1.Location = New System.Drawing.Point(191, 124)
-        Me.VS7_Textbox1.Margin = New System.Windows.Forms.Padding(4)
-        Me.VS7_Textbox1.Name = "VS7_Textbox1"
-        Me.VS7_Textbox1.PLC_Bit = 0
-        Me.VS7_Textbox1.PLC_Byte = 400
-        Me.VS7_Textbox1.PLC_DataArea = Proyecto_ViSnap7.General.DataArea.MARK
-        Me.VS7_Textbox1.PLC_DataType = Proyecto_ViSnap7.General.DataType.INT
-        Me.VS7_Textbox1.PLC_DB = 1
-        Me.VS7_Textbox1.PLC_FormActive = True
-        Me.VS7_Textbox1.PLC_FormNumber = 1
-        Me.VS7_Textbox1.PLC_Length = 0
-        Me.VS7_Textbox1.PLC_Number = 0
-        Me.VS7_Textbox1.Size = New System.Drawing.Size(132, 22)
-        Me.VS7_Textbox1.TabIndex = 1
-        '
-        'ViSnap7_LoadSaveForm1
-        '
-        Me.ViSnap7_LoadSaveForm1.Extension = "dat"
-        Me.ViSnap7_LoadSaveForm1.LoadForm = False
-        Me.ViSnap7_LoadSaveForm1.Location = New System.Drawing.Point(359, 196)
-        Me.ViSnap7_LoadSaveForm1.Margin = New System.Windows.Forms.Padding(4)
-        Me.ViSnap7_LoadSaveForm1.Name = "ViSnap7_LoadSaveForm1"
-        Me.ViSnap7_LoadSaveForm1.PLC_FormNumber = 1
-        Me.ViSnap7_LoadSaveForm1.SaveFolder = "C:\recipes"
-        Me.ViSnap7_LoadSaveForm1.Size = New System.Drawing.Size(100, 28)
-        Me.ViSnap7_LoadSaveForm1.TabIndex = 0
-        Me.ViSnap7_LoadSaveForm1.Text = "ViSnap7_LoadSaveForm1"
-        '
-        'VS7_Checkbox1
-        '
-        Me.VS7_Checkbox1.AutoSize = True
-        Me.VS7_Checkbox1.Location = New System.Drawing.Point(399, 69)
-        Me.VS7_Checkbox1.Name = "VS7_Checkbox1"
-        Me.VS7_Checkbox1.PLC_Bit = 0
-        Me.VS7_Checkbox1.PLC_Byte = 0
-        Me.VS7_Checkbox1.PLC_DataArea = Proyecto_ViSnap7.General.DataArea.DB
-        Me.VS7_Checkbox1.PLC_DataType = Proyecto_ViSnap7.General.DataType.BOOL
-        Me.VS7_Checkbox1.PLC_DB = 0
-        Me.VS7_Checkbox1.PLC_FormActive = True
-        Me.VS7_Checkbox1.PLC_FormNumber = 1
-        Me.VS7_Checkbox1.PLC_Length = 0
-        Me.VS7_Checkbox1.PLC_Number = 0
-        Me.VS7_Checkbox1.Size = New System.Drawing.Size(133, 21)
-        Me.VS7_Checkbox1.TabIndex = 6
-        Me.VS7_Checkbox1.Text = "VS7_Checkbox1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1131, 679)
-        Me.Controls.Add(Me.VS7_Checkbox1)
-        Me.Controls.Add(Me.VS7_Textbox4)
-        Me.Controls.Add(Me.ViSnap7_LoadSaveForm2)
-        Me.Controls.Add(Me.VS7_Textbox3)
-        Me.Controls.Add(Me.VS7_Textbox2)
-        Me.Controls.Add(Me.VS7_Textbox1)
-        Me.Controls.Add(Me.ViSnap7_LoadSaveForm1)
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents VS7_ValueLabel1 As VS7_ValueLabel
-    Friend WithEvents ViSnap7_LoadSaveForm1 As ViSnap7_LoadSaveForm
-    Friend WithEvents VS7_Textbox1 As VS7_Textbox
-    Friend WithEvents VS7_Textbox2 As VS7_Textbox
-    Friend WithEvents VS7_Textbox3 As VS7_Textbox
-    Friend WithEvents ViSnap7_LoadSaveForm2 As ViSnap7_LoadSaveForm
-    Friend WithEvents VS7_Textbox4 As VS7_Textbox
-    Friend WithEvents VS7_Checkbox1 As VS7_Checkbox
 End Class
