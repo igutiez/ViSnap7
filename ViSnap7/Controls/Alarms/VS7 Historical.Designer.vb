6852252ba97af7bf42c3e649bf6af30400544795
igutiez <igutiez@gmail.com>
2021-09-13 23:15:27 +0200

    Añadido histórico de alarmas

---
+++ ViSnap7/Controls/Alarms/VS7 Historical.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class VS7_Historical
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.HistoricalText = New System.Windows.Forms.RichTextBox()
+        Me.DateSelector = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'HistoricalText
+        '
+        Me.HistoricalText.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.HistoricalText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.HistoricalText.Location = New System.Drawing.Point(23, 64)
+        Me.HistoricalText.Name = "HistoricalText"
+        Me.HistoricalText.ReadOnly = True
+        Me.HistoricalText.Size = New System.Drawing.Size(1078, 290)
+        Me.HistoricalText.TabIndex = 0
+        Me.HistoricalText.Text = ""
+        '
+        'DateSelector
+        '
+        Me.DateSelector.Location = New System.Drawing.Point(23, 36)
+        Me.DateSelector.Name = "DateSelector"
+        Me.DateSelector.Size = New System.Drawing.Size(200, 22)
+        Me.DateSelector.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(23, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(190, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Historical date selection"
+        '
+        'VS7_Historical
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DateSelector)
+        Me.Controls.Add(Me.HistoricalText)
+        Me.Name = "VS7_Historical"
+        Me.Size = New System.Drawing.Size(1129, 372)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents HistoricalText As RichTextBox
+    Friend WithEvents DateSelector As DateTimePicker
+    Friend WithEvents Label1 As Label
+End Class
---
+++ ViSnap7/Controls/Alarms/VS7 Historical.vb
@@ -0,0 +1,137 @@
+﻿Imports System.Windows.Forms.Design
+<System.ComponentModel.Designer(GetType(HistoricalControllerDesigner))>
+Public Class VS7_Historical
+    Private _folderExplorer As String
+    <System.ComponentModel.Category(KPlcPropertiesCategory), System.ComponentModel.Description(KFolderLog)>
+    Public Property PLC_SaveFolder As String
+        Get
+            Return _folderExplorer
+        End Get
+        Set(value As String)
+            _folderExplorer = value
+        End Set
+    End Property
+
+
+    Private Sub DateTimePicker_ValueChanged(sender As Object, e As EventArgs) Handles DateSelector.ValueChanged
+        Try
+            Dim fileDate As String = DateSelector.Value.ToShortDateString.Replace("/", "-")
+            Dim filePath = Me.PLC_SaveFolder + "\" + fileDate + ".txt"
+            Dim texts() = IO.File.ReadAllLines(filePath)
+
+            Me.HistoricalText.Text = "HOUR" + vbTab + "N#" + vbTab + "STATUS" + vbTab + "ALARM TEXT" + vbCrLf + vbCrLf
+            For Each line As String In texts
+                If line.Contains("ACK") Then
+
+                End If
+                Me.HistoricalText.Text = HistoricalText.Text + line + vbCrLf
+
+            Next
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub VS7_Historical_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+#Region "Label Smart tags"
+
+Public Class HistoricalControllerDesigner
+    Inherits ControlDesigner
+
+    Private _actionListCollection As DesignerActionListCollection
+
+    Public Overrides ReadOnly Property ActionLists() As System.ComponentModel.Design.DesignerActionListCollection
+        Get
+            If _actionListCollection Is Nothing Then
+                _actionListCollection = New DesignerActionListCollection()
+                _actionListCollection.Add(New HistoricalControllerActionList(Me.Control))
+            End If
+
+            Return _actionListCollection
+        End Get
+    End Property
+End Class
+
+Friend Class HistoricalControllerActionList
+    Inherits DesignerActionList
+
+    Private ctr As VS7_Historical
+    Private designerActionSvc As DesignerActionUIService
+
+    Public Sub New(ByVal component As IComponent)
+        MyBase.New(component)
+
+        ctr = DirectCast(component, VS7_Historical)
+        designerActionSvc = CType(GetService(GetType(DesignerActionUIService)), DesignerActionUIService)
+    End Sub
+
+#Region " Properties to display in the Smart-Tag panel "
+
+
+
+    Public Property PLC_SaveFolder() As String
+        Get
+            Return ctr.PLC_SaveFolder
+        End Get
+        Set(ByVal value As String)
+
+            GetPropertyByName(ctr, "PLC_SaveFolder").SetValue(ctr, value)
+
+        End Set
+    End Property
+
+#End Region
+
+#Region " Methods to display in the Smart-Tag panel "
+
+    Private Sub SelectFolder()
+        Dim FolderSelector As New FolderBrowserDialog
+        If FolderSelector.ShowDialog = DialogResult.OK Then
+            GetPropertyByName(ctr, "PLC_SaveFolder").SetValue(ctr, FolderSelector.SelectedPath)
+            designerActionSvc.Refresh(ctr)
+
+        End If
+    End Sub
+
+
+
+
+    Public Sub OnDock()
+        If ctr.Dock = DockStyle.Fill Then
+            ctr.Dock = DockStyle.None
+        Else
+            ctr.Dock = DockStyle.Fill
+        End If
+
+        designerActionSvc.Refresh(ctr)
+    End Sub
+
+#End Region
+
+    Public Overrides Function GetSortedActionItems() As System.ComponentModel.Design.DesignerActionItemCollection
+        Dim str As String
+        Dim items As New DesignerActionItemCollection
+
+        If ctr.Dock = DockStyle.Fill Then
+            str = "Undock in parent container."
+        Else
+            str = "Dock in parent container."
+        End If
+
+        'Add a few Header Items (categories)
+        items.Add(New DesignerActionHeaderItem(KPlcAdressingCategory))
+
+        'Add the properties
+        items.Add(New DesignerActionPropertyItem("PLC_SaveFolder", KSaveFolder, KPlcFolderCategory, KFolderSaveTip))
+        items.Add(New DesignerActionMethodItem(Me, "SelectFolder", KSelectFolder))
+
+
+        'Return the ActionItemCollection
+        Return items
+    End Function
+End Class
+
+#End Region
