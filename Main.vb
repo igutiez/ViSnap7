94b955e148cc5224909adf9d5ac9a1001e30c646
igutiez <igutiez@gmail.com>
2021-09-24 19:42:21 +0200

    Fixed SetSIntAt. Negative values can be used.

--- Main.vb
+++ Main.vb
@@ -1,9 +1,6 @@
-﻿
-Public Class Main
+﻿Public Class Main
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'DO NOT CHANGE THIS LINE
         General.LaunchCommunications()
     End Sub
-
-
 End Class
